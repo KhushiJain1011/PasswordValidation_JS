3b8fbe32b3895baaa14ec534523a0d46bb8f2114
Khushi Jain <khushij1011@gmail.com>
2024-01-28 20:05:10 +0530

    Changes

--- style.js
+++ style.js
@@ -6,36 +6,27 @@ let password = document.getElementById("psw")
 let lower_letter = /[a-z]/
 let upper_letter = /[A-Z]/
 let number = /[0-9]/
-let flag = 1;
+// let flag = 1;
 
-function validateForm() {
+function validatePassword() {
     if (password.value.length < 8) {
         document.getElementById("passError").innerHTML = "Minimum length of password must be 8"
-        flag = 0
-    }
-    else if (password.value.length >= 8) {
-        document.getElementById("passError").innerHTML = ""
-        flag = 1;
     }
 
-    if (!password.value.match(lower_letter)) {
+    else if (!password.value.match(lower_letter)) {
         document.getElementById("passError").innerHTML = "Enter atleast one lowercase letter"
-        flag = 0;
     }
 
-    if (!password.value.match(upper_letter)) {
+    else if (!password.value.match(upper_letter)) {
         document.getElementById("passError").innerHTML = "Enter atleast one uppercase letter"
-        flag = 0;
     }
 
-    if (!password.value.match(number)) {
+    else if (!password.value.match(number)) {
         document.getElementById("passError").innerHTML = "Enter atleast one digit"
-        flag = 0;
+    }
+    else{
+        document.getElementById("passError").innerHTML = ""
+        alert("Valid password")
     }
 
-    if (flag) {
-        return true;
-    } else {
-        return false;
-    }
 }
